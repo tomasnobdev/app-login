927d103537231be7704161efdb4a7a35151f61f2
tomasnobile <tnobile6@gmail.com>
2020-07-02 21:08:49 -0300

    Make a first part of Crud...

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,5 +1,33 @@
 const router = require('express').Router();
 
+const Note = require('../models/Note');
+
+router.get("/notes/add", (req, res) => {
+	res.render('notes/new-note');
+});
+
+router.post('/notes/new-note', async (req, res) => {
+	const { title, description } = req.body;
+	const errors = [];
+	if(!title) {
+		errors.push({text: 'Please write a Title'});
+	}
+	if (!description) {
+		errors.push({text: 'Please write a Description'});
+	}
+	if(errors.length > 0) {
+		res.render('notes/new-note', {
+			errors,
+			title,
+			description
+		});
+	} else {
+	const newNote = new Note({ title, description });
+	await newNote.save();
+	res.redirect('/notes');
+}
+});
+
 router.get('/notes', (req, res) => {
 	res.send('Notes from database');
 });
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
 router.get('/users/signin', (req, res) => {
-	res.send('Ingresando a la app');
+	res.render('users/signin');
 });
 
 router.get('/users/signup', (req, res) => {
-	res.send('Formulario de authenticacion');
+	res.render('users/signup');
 });
 module.exports = router;
